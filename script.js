7ade1b0ec7e9a10ee4ba1c6c476a65b75a6ab7be
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-08-16 15:51:20 +0530

    JS step-1 completed

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('.equal');
+const clearAllButton = document.querySelector('.clear-all');
+const backspaceButton = document.querySelector('.backspace');
+const history = document.querySelector('.history');
+const result = document.querySelector('.result');
+
 let currentNumber = 0;
 let previousNumber = 0;
 let operator = null;
-let historyText = "";
-let history = document.querySelector(".history");
-let result = document.querySelector(".result");
+let historyText = "0";
 
 function addNumber(number) {
-    currentNumber = currentNumber * 10 + number;
+    if (currentNumber == 0) {
+        currentNumber = number;
+    } else {
+        currentNumber = currentNumber + number;
+    }
     result.innerHTML = currentNumber;
 }
 
@@ -29,6 +39,11 @@ function calculate() {
     } else if (operator == "*") {
         currentNumber = previousNumber * currentNumber;
     } else if (operator == "/") {
+        if(currentNumber == 0) {
+            alert("Cannot divide by zero");
+            clearAll();
+            return;
+        }
         currentNumber = previousNumber / currentNumber;
     }
     result.innerHTML = currentNumber;
@@ -41,7 +56,7 @@ function clearAll() {
     currentNumber = 0;
     previousNumber = 0;
     operator = null;
-    historyText = "";
+    historyText = "0";
     history.innerHTML = historyText;
     result.innerHTML = currentNumber;
 }
@@ -55,3 +70,27 @@ function backspace() {
     currentNumber = Math.floor(currentNumber / 10);
     result.innerHTML = currentNumber;
 }
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        addNumber(button.innerHTML);
+    })
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        addOperator(button.innerHTML);
+    })
+})
+
+equalButton.addEventListener('click', () => {
+    calculate();
+})
+
+clearAllButton.addEventListener('click', () => {
+    clearAll();
+})
+
+backspaceButton.addEventListener('click', () => {
+    backspace();
+})
